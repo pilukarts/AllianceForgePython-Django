f4598011579e88c9cd5d8302b5d9563ff90d1ea3
pilukarts <pilarrodriguezcastillejo@gmail.com>
2025-05-16 17:39:07 +0100

    AllianceForgePython

---
+++ .history/allianceforge/allianceforge/urls_20250515235423.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for allianceforge project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]# In my_app/urls.py (replace 'my_app' with your app's name)
+
+from django.urls import path
+from . import views # Import your app's views
+
+urlpatterns = [
+    # ... other urls for your app ...
+
+    # URL to receive the wallet address from the frontend
+    # **IMPORTANT:** This URL path 'save-wallet-address/' MUST match
+    # the URL you put in the fetch function in the JavaScript
+    path('save-wallet-address/', views.save_wallet_address, name='save_wallet_address'),
+
+    # ... other urls for your app ...
+]
